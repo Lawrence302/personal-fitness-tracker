0348fd13d226a13dd63602f74cf348559e8004c4
Lawrence <ganjeblerence@gmail.com>
2025-12-31 03:16:17 +0530

    finished basic home page design without any functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
           <Dumbbell className='w-8 h-8 ' size={32} color='white' />
         </div>
         <div>
-          <h3 className='text-xl md:text-2xl lg:text-2xl text-white font-bold'>
+          <h3 className='text-sm md:text-xl lg:text-2xl text-white font-bold tracking-tighter italic'>
             MY CALI TRACK
           </h3>
           <p className='text-xs md:text-sm lg:text-base text-zinc-500 font-bold'>
@@ -16,19 +16,19 @@ const Navbar = () => {
           </p>
         </div>
       </div>
-      <div className='border divide-x-4 divide-zinc-900 flex text-zinc-500 p-2 font-bold text-xs justify-end'>
+      <div className='border-b border-zinc-900 divide-x-4 divide-zinc-900 flex text-zinc-500 p-2 font-bold text-xs justify-end'>
         <div className='pr-2 text-right'>
           <p>SESSIONS</p>
           <div className='flex text-sm text-cyan-400 items-center justify-end gap-1'>
             <CircleCheck className='h-3 w-3 align-middle' strokeWidth={2.25} />
-            <span className=''>1</span>
+            <span className='text-xs md:text-sm'>1</span>
           </div>
         </div>
         <div className='pl-2 text-right'>
           <p>STREAK</p>
           <div className='flex items-center gap-1 text-orange-400'>
             <Flame className='h-3 w-3 align-middle' />
-            <span className='text-sm'>5 DAYS</span>
+            <span className='text-xs md:text-sm'>5 DAYS</span>
           </div>
         </div>
       </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,9 +2,9 @@ import { House, Calendar, Search, History, BrainCircuit } from "lucide-react";
 
 const sidebar = () => {
   return (
-    <div className='w-24 h-full border border-2 border-r-zinc-900 '>
-      <div className=' h-full'>
-        <ul className='border h-full text-zinc-600 p-6 flex flex-col  justify-around items-center text-xs'>
+    <div className='fixed bottom-0 left-0 z-50 md:static w-full h-20 md:w-24 md:h-full border-r  border-zinc-900 bg-zinc-950 '>
+      <div className=' md:h-full'>
+        <ul className='border h-full text-zinc-600 p-6 flex flex-row md:flex-col  justify-around items-center text-xs'>
           <li className='flex  flex-col items-center hover:text-white cursor-pointer'>
             <House size={24} strokeWidth={2.25} />
             <p>Home</p>
