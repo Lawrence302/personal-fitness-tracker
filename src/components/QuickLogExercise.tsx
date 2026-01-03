66e92037f0552d08762697e99ffe0f6dd2f69608
Lawrence <ganjeblerence@gmail.com>
2026-01-03 23:46:00 +0530

    created QuickLogExercise.tsx and transfered the quick log component to its own file

---
+++ src/components/QuickLogExercise.tsx
@@ -0,0 +1,32 @@
+import { Plus } from "lucide-react";
+//
+const exercises = [
+  "Pull-ups",
+  "air-squads",
+  "dead-hang",
+  "horse-stance",
+  "forward-lunges",
+];
+
+const QuickLogExercise = () => {
+  return (
+    <div>
+      <h2 className='font-bold py-4'>QUICK LOG EXERCISE</h2>{" "}
+      <div className='flex flex-wrap text-white gap-2'>
+        {exercises.map((exercise, index) => {
+          return (
+            <button
+              key={index}
+              className='bg-zinc-900 border border-zinc-800 px-4 py-2 rounded-xl font-bold hover:border-cyan-500/50 flex gap-2 items-center text-sm '
+            >
+              <Plus className='text-cyan-500' />
+              {exercise}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default QuickLogExercise;
