ae73c8205328c65bc3e3a5b55b5e9b59bf2f6f91
Lawrence <ganjeblerence@gmail.com>
2026-01-24 00:56:29 +0530

    added manifest.json, icon images and basic setup for app to work as pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,38 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "MY Cali Track",
+        short_name: "CaliTrack",
+        description:
+          "Personal Calisthenics Fitness Tracker: Track your bodyweight workouts, log exercises, earn points, and monitor progress. All without any equipment!",
+        theme_color: "#18181b",
+        background_color: "#09090b",
+        display: "standalone",
+        start_url: ".",
+        icons: [
+          {
+            src: "./android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/personal-fitness-tracker/",
 });
