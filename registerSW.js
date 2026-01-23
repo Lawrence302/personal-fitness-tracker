53a45c83d7f44cfe6e64576e9c82367a6944eec4
Lawrence302 <Lawrence302@users.noreply.github.com>
2026-01-23 19:27:24 +0000

    deploy: ae73c8205328c65bc3e3a5b55b5e9b59bf2f6f91

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/personal-fitness-tracker/sw.js', { scope: '/personal-fitness-tracker/' })})}
